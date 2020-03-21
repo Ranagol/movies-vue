f1a6ab14cfa1f5cc0bf2514f1a430191b38f12fc
Andor <andorhorvat@gmail.com>
2020-03-21 19:55:13 +0100

    Add movie sorted

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -1,11 +1,48 @@
 <template>
   <div>
-    <p>AddMovie.vue</p>
+    <h4>Add new movie</h4>
+    <form @submit.prevent="onSubmit">
+      <input v-model="title" type="text" name="title" placeholder="title" class="form-control">
+      <input v-model="director"  type="text" name="director" placeholder="director" class="form-control">
+      <input v-model="imageUrl"  type="url" name="imageUrl" placeholder="imageUrl" class="form-control">
+      <input v-model="duration"  type="number" name="duration" placeholder="duration" class="form-control">
+      <input v-model="releaseDate"  type="date" name="releaseDate" placeholder="releaseDate" class="form-control">
+      <input v-model="genre"  type="text" name="genre" placeholder="genre" class="form-control">
+      <button type="submit" class="btn btn-success">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
+import movieService from '../services/movieService';
 export default {
   name: 'AddMovie',
+  data(){
+    return {
+      movie:'',
+      title: '',
+      director: '',
+      imageUrl: '',
+      duration: '',
+      releaseDate: '',
+      genre: '',
+
+    }
+  },
+  methods: {
+    async onSubmit(){
+      let body = {
+        title: this.title,
+        director: this.director,
+        imageUrl: this.imageUrl,
+        duration: this.duration,
+        releaseDate: this.releaseDate,
+        genre: this.genre,
+      }
+      await movieService.createMovie(body);
+      body = {};
+      this.$router.push('/movies');
+    }
+  }
 }
 </script>
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,11 +1,39 @@
 <template>
   <div>
-    <p>Movies.vue</p>
+    <table class="table">
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Director</th>
+        <th>url</th>
+        <th>Duration</th>
+        <th>Release date</th>
+        <th>Genre</th>
+      </tr>
+      <tr v-for="movie in movies" :key="movie.id">
+        <td>{{ movie.id }}</td>
+        <td>{{ movie.title }}</td>
+        <td>{{ movie.director }}</td>
+        <td>{{ movie.imageUrl }}</td>
+        <td>{{ movie.duration }}</td>
+        <td>{{ movie.releaseDate }}</td>
+        <td>{{ movie.genre }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import movieService from '../services/movieService';
 export default {
   name: 'Movies',
+  data(){
+    return {
+      movies: [],
+    }
+  },
+  async created(){
+    this.movies = await movieService.getAll();
+  }
 }
 </script>
